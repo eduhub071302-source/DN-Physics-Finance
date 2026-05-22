cf5d6aefd88f5b1e08e4df43caa44c70042e9cd8
DN_Physics <eduhub071302@gmail.com>
2026-05-22 16:33:37 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// TODO: Paste your Firebase Config object here
 const firebaseConfig = {
   apiKey: "AIzaSyCtGfUOC7p_RyP6hEgXjJ6zWJ6UE6H7QWk",
   authDomain: "dn-physics-finance.firebaseapp.com",
@@ -15,7 +14,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Handle UI switching based on Auth State
 onAuthStateChanged(auth, (user) => {
     if (user) {
         document.getElementById('auth-container').classList.add('hidden');
@@ -26,15 +24,12 @@ onAuthStateChanged(auth, (user) => {
     }
 });
 
-// Google Login
 document.getElementById('google-login-btn').addEventListener('click', () => {
     signInWithPopup(auth, provider).catch(error => alert(error.message));
 });
 
-// Logout
 document.getElementById('logout-btn').addEventListener('click', () => {
     signOut(auth);
 });
 
-// Export these so db.js can use the same connection
 export { app, auth };
